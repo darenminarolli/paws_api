d51299c62d848f9e31f2069f6751ce040849af23
Daren <darenminarolli@gmail.com>
2024-04-30 17:23:25 +0200

    another try

--- routes/owner.route.js
+++ routes/owner.route.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const path = require('path');
+const os = require('os');
 
 const { getOwners, getSingleOwner, postOwner, updateOwner, deleteOwner } = require('../controllers/owner.controller.js');
 
